732d61e054f0c5da2ce6bcab971653c84891ce7e
Rory Byrne <roryofbyrne@gmail.com>
2017-11-05 01:11:44 +0000

    [Code] Removed Neuron model since it's not being used anymore

--- src/model/neuron/weighted_neuron.py
+++
@@ -1,7 +0,0 @@
-from model.neuron import Neuron
-
-class WeightedNeuron(Neuron):
-    def __init__(self, weights):
-        Neuron.__init__(self)
-        self.input = None
-        self.weights = weights
